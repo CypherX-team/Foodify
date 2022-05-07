e979c0a46260cf93d3f5d2e8e22188c772076379
build-error <63505435+build-error@users.noreply.github.com>
2022-05-07 13:19:35 +0530

    feature: added registration page

---
+++ static/js/signup.js
@@ -0,0 +1,34 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+// Email Validation
+const ValidateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+  
+  const validate = () => {
+    const $result = $('#result');
+    const email = $('#email').val();
+    $result.text('');
+  
+    if (validateEmail(email)) {
+      $result.text(email + ' is valid :)');
+      $result.css('color', 'green');
+    } else {
+      $result.text(email + ' is not valid :(');
+      $result.css('color', 'red');
+    }
+    return false;
+  }
+  
