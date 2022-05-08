c2b87868d11c0a489d2889640eac789ab56169dd
build-error <63505435+build-error@users.noreply.github.com>
2022-05-08 09:26:35 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('templates/about.html', views.about, name='about'),
     path('templates/contact.html', views.contact, name='contact'),
     path('templates/donate.html', views.donate, name='donate'),
-    path('templates/receive.html', views.receive, name='receive'),
+    path('templates/recieve.html', views.recieve, name='recieve'),
     path('templates/signup.html', views.signup, name='signup')
 ]
