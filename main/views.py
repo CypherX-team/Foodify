1d4fb9f416fca3b2102a7a4486ab2682fbefc5c7
build-error <63505435+build-error@users.noreply.github.com>
2022-05-08 08:30:47 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
-    
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')    
+
+def donate(request):
+    return render(request, 'donate.html')
+
+def receive(request):
+    return render(request, 'receive.html')
+
+def signup(request):
+    return render(request, 'signup.html')
