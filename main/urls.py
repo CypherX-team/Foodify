17748f19e376ae791ca738b9482e66c55dbf171b
build-error <63505435+build-error@users.noreply.github.com>
2022-05-08 08:29:57 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('templates/index.html', views.index, name='index'),
+    path('templates/about.html', views.about, name='about'),
+    path('templates/contact.html', views.contact, name='contact'),
+    path('templates/donate.html', views.donate, name='donate'),
+    path('templates/receive.html', views.receive, name='receive'),
+    path('templates/signup.html', views.signup, name='signup')
 ]
